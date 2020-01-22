f364a51b9c1971ecfc91e6bb068eeb6f53ec021a
root <root@instance-1.us-central1-a.c.arched-aleph-230412.internal>
2020-01-22 12:41:19 +0000

    push

--- downloader_benzi.sh
+++ downloader_benzi.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 echo "Download starto!"
 
+count=0
 for file in `ls /root/bt`
 do
+	let count++
 	#cat /root/bt/$file
+	if [ $(($count%100)) = '0' ];then
+	sleep 86400
+	fi
+	sleep 0.5
 	nohup aria2c -T /root/bt/$file >/dev/null 2>&1 &
 done
